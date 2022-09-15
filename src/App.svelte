46ed7f1795f0b4102ba075df00d8e4a41af24635
Andrew Jordaan <andrew.jordaan@platform45.com>
2022-09-15 22:31:48 +0200

    increase animation time

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,7 @@
   import WordFinder from "./WordFinder";
 
   const [send, receive] = crossfade({
-    duration: (d) => Math.sqrt(d * 4000),
+    duration: (d) => Math.sqrt(d * 3000),
 
     fallback(node, params) {
       const style = getComputedStyle(node);
